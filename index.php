34d194bbf26ee56cb84581d93874c81ebb6dc2f7
juradrugiDomaWin10 <drazovko@foi.hr>
2017-01-08 15:03:07 +0100

    Prvi commit
    sve promjene idu u prvu verziju
    
    zadatak: prvi

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo '<h2 align="center">Testiranje Gita Notepad++!</h2>';
+        ?>
+    </body>
+</html>
